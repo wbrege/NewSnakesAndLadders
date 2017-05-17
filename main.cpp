7fe7e7e2c12f740c7a4a7a5dbfadc9d7942336c2
William Brege <william.brege@gmail.com>
2017-05-16 18:25:01 -0700

    Basic code files added

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/** @file main.cpp
+    @author William Brege
+    @date 05/16/2017
+ 
+    This is a recreation of the children's game "Snakes and Ladders", with some creative additions
+ */
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
---
+++ snakesAndHeaders.h
@@ -0,0 +1,29 @@
+/** @file snakesAndHeaders.h
+    @author William Brege
+    @date 05/16/2017
+ 
+    This contains the classes for my "Snakes and Ladders" reboot
+ */
+
+#ifndef snakesAndHeaders_h
+#define snakesAndHeaders_h
+
+/** The tiles form the basis of the entire game
+ */
+class tile{
+    
+};
+
+/** The "board" is essentially just a manager for the tiles
+ */
+class board{
+    
+};
+
+/** 
+ */
+class player{
+    
+};
+
+#endif /* snakesAndHeaders_h */
